23a06f025013ca5156b3b870930c9e2960c74725
asahmed <abdel.samea@hotmail.com>
2021-10-21 23:36:25 +0200

    py file update

---
+++ 21-Oct-21/ext01.py
@@ -0,0 +1,32 @@
+# create program for bookmark
+# recieve website and print out the left
+#  number remainning to add anothe webite
+# and finally print all web site in bookmark
+# bookmark = []
+# index = 5
+# website = input("plz enter website: ")
+# while index < len(bookmark):
+    
+#     bookmark[index].append(website)
+#     index -= 1
+# else:
+#     print("your bookmark is full")
+
+#  -------------------------------
+# ex
+# i = 1
+# while i < 6:
+#   print(i)
+
+#   i += 1
+
+# -------------------------------------
+# ex
+l1 = []
+index = 5
+
+while index > 0:
+    newl = input('plz enter number: ')
+    l1.append(newl)
+    index -= 1
+print(l1)
