4a3ccee9438e1e55cf66cedf03c58c5544d47365
asahmed <abdel.samea@hotmail.com>
2022-07-19 21:33:08 +0200

    concatenation

---
+++ 19-July-2022/concatenation.py
@@ -0,0 +1,33 @@
+# -------------------
+# -- Concatenation --
+# -------------------
+
+firstname = 'Abdle Rahman'
+lastname = 'Ali'
+fullname = firstname + ' ' + lastname
+print(f"first name is: {firstname}")
+print(f"last name is: {lastname}")
+print(f"full name is: {fullname}")
+
+print("*"* 50)
+
+# ---------------------------------
+
+msg = "I Love"
+lang = "Python"
+print(msg + " " + lang)
+
+full = msg + " " + lang
+print(full)
+
+a = "First \
+Second \
+Third"
+
+b = "A \
+B \
+C"
+
+print(a + "\n" + b)
+
+print("Hello " + 1)  # Error
